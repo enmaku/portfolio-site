3561943860ca247de8a43a20b387ba78bfdd036b
David Perry <enmaku@gmail.com>
2026-05-16 09:35:30 -0700

    moved movie timer to firebase backend

---
+++ src/features/movie-vote/p2p/sessionRoomRtdb.js
@@ -0,0 +1,17 @@
+/** RTDB room children removed when a new host claims an idle suffix (not on resume/reconnect). */
+export const ROOM_CLAIM_RESET_PATHS = Object.freeze([
+  'inbox',
+  'welcome',
+  'guestOnline',
+  'state',
+  'ended',
+  'hostVisible',
+])
+
+/**
+ * @param {unknown} endedSnapVal RTDB `ended` node value.
+ * @returns {boolean}
+ */
+export function isRoomMarkedEnded(endedSnapVal) {
+  return endedSnapVal != null
+}
